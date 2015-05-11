82a01a9b1c44efbc39fd951f0f0a21613a096fb9
Nithin Tumma <nithin@Nithins-MacBook-Pro.local>
2015-05-11 13:51:25 -0400

    fixed bugs

--- simple_graph.py
+++ simple_graph.py
@@ -41,7 +41,6 @@ def getPotentialOverlap(fromItem, toItem, incoming_vertices, outgoing_vertices, 
         return 0
 
 
-
 def testPotentialOverlap(graph=None, l = 100, w = 100, degree = 100, r = 200, n = 1):
     start = time.time()
     
@@ -114,6 +113,8 @@ def simulation(graph=None, l = 100, w = 100, degree = 100, r = 200, fire_thresh=
 
     #holds associations for key fromItem to a list of toItems
     associations = {}
+    #keys tuples of memorizated items, value is the memorized conjunction
+    memorizations = {}
 
     #creates random items
     for i in range(num_items):
@@ -132,7 +133,8 @@ def simulation(graph=None, l = 100, w = 100, degree = 100, r = 200, fire_thresh=
 
         for fromItemVertex in fromItemVertices:
             for toItemVertex in toItemVertices:
-                weights[(fromItemVertex, toItemVertex)] = max_weight
+                if (fromItemVertex, toItemVertex) in weights:
+                    weights[(fromItemVertex, toItemVertex)] = max_weight
 
 
     elapsed = time.time() - start
@@ -160,12 +162,36 @@ def simulation(graph=None, l = 100, w = 100, degree = 100, r = 200, fire_thresh=
         firing_items = set(getActiveItems(firing_vertices, items))
         print len(firing_vertices), len(associated_items), len(firing_items.intersection(associated_items)), len(firing_items)
 
+# how do we determine if we should be activating a memorization?
+# alternatively, in each timestep we could simulate a few things, i.e, particular associations and memorizations
+
+def simulate_memorization_on(graph, weights, item1, item2, mem_item, items, r, fire_thresh=0.85)
+    """
+    we assume that mem_item is stored as the meorization of item1 and item2
+    will activate the nodes in item1 and item2, see if we activate nodes in mem_item
+    only deals with ON error currently
+    """
+    # activate neurons of both item sets
+    firing_vertices = set.union(fire_item(item1, fire_thresh), fire_item(item2, fire_thresh))
+    firing_vertices = get_active_set((incoming_vertices, outgoing_vertices), firing_vertices, weights)
+    firing_items = getActiveItems(firing_vertices)
+    if mem_item in firing_items: 
+        return True
+    else:
+        return False
+
+def simulate_memorization_off(graph, weights, item1, item2, mem_item, items, r, fire_thresh=0.85):
+    """ 
+    simulate memorization with 
+    """
+    return None
 
 
-def get_active_set(graph, firing_vertices, weights, threshold = 50):
+def get_active_set(graph, firing_vertices, weights = False, threshold = 50):
     """
     runs the simulation for one time point, with the firing_vertices being the list/set of nodes that are active in the current time point
     threshold, min number of active neighbors
+    weights: if false, defaults to 1
     returns the set of nodes that are firing at next time point
     """
     incoming_vertices, outgoing_vertices =  graph
@@ -174,12 +200,16 @@ def get_active_set(graph, firing_vertices, weights, threshold = 50):
     for vert in firing_vertices:
         for cur_vert in outgoing_vertices[vert]:
             if cur_vert not in next_firing:
-                #TODO add weight logic
-                # thresh[cur_vert] += weight_ougoing_vertices[vert][cur_vert]
                 try: 
-                    thresh[cur_vert] += weights[(vert, cur_vert)]
+                    if weights:
+                        thresh[cur_vert] += weights[(vert, cur_vert)]
+                    else:
+                        thresh[cur_vert] += 1
                 except KeyError:
-                    thresh[cur_vert] = weights[(vert, cur_vert)]
+                    if weights:
+                        thresh[cur_vert] = weights[(vert, cur_vert)]
+                    else:
+                        thresh[cur_vert] = 1
                 if thresh[cur_vert] >= threshold:
                     next_firing.add(cur_vert)
     return next_firing
@@ -196,11 +226,11 @@ def test_memorization(graph, item1, item2, items, r):
     returns list of nodes that can be activated by item1 and item 2
     if not possible, returns None
     """
-    item1_actives = get_active_set(graph, fire_item(items[item1]))
-    item2_actives = get_active_set(graph, fire_item(items[item2]))
+    item1_actives = get_active_set(graph, fire_item(items[item1]), weights=False, threshold=5)
+    item2_actives = get_active_set(graph, fire_item(items[item2]), weights=False, threshold=5)
     candidate_verts = intersection(item1_actives, item2_active)
     if len(candidate_verts) >= r:
-        return candidate_verts
+        return candidate_verts 
     else:
         return None
 
@@ -215,10 +245,92 @@ def memorize(graph, item1, item2, items, r):
     else:
         return None
 
+def make_memorization(graph, weights, item1, item2, items, r):
+    """
+    Finds a set of r vertices connected to items1 and 2, creates a new item 
+    """
+    candidate_verts = memorize(graph, item1, item2, items, r)
+    if candidate_verts:
+        mem_item = candidate_verts
+        items.append(mem_item)
+        mem_item_index = len(items)-1 
+        for item_vert in set.union(items[item1], items[item2]):
+            for mem_vert in candidate_verts:
+                if (item_vert, mem_vert) in weights:
+                    weights[(item_vert, mem_vert)] = max_synapse_strength
+    else:
+        # memorization was not possible for these two items
+        return None
+    return graph, weights, mem_item_index, items
+
+
+
 def test_association(graph, item1, item2, items):
-    item1_actives = get_active_set(graph, items[item1])
+    item1_actives = get_active_set(graph, items[item1], weights=False, threshold=5)
     if item2 in get_active_items(item1_actives, items):
         return True
     else:
         return False
-     
+
+
+def get_memorization_overlap(fromItem, toItem, incoming_vertices, outgoing_vertices, items, maxSynapseStrength=1.0, threshold=5.0):
+    neurons_from = items[fromItem]
+    neurons_to = items[toItem]
+
+    number_connected = 0.0
+    total_number = 0
+
+    for neuron in neurons_to:
+        #first checks if this neuron is in both items
+        if neuron in neurons_from:
+            number_connected += 1
+        else:
+            #gets the list of connections to this neuron in the toItem
+            connections = incoming_vertices[neuron]
+
+            #gets the list of connections to this neuron from a neuron in fromItem
+            shared_connections = [x for x in connections if x in neurons_from]               
+
+            #if the potential weight is large enough to activate the threshold
+            if len(shared_connections) >= threshold/maxSynapseStrength:
+                number_connected += 1
+
+            total_number += len(shared_connections)
+
+    try:
+        return number_connected / len(neurons_to)
+    except ZeroDivisionError:
+        print "Error"
+        return 0
+
+def test_memorization_overlap(graph=None, l = 100, w = 100, degree = 100, r = 200, n = 1):
+    start = time.time()
+    
+    if graph:
+        incoming_vertices, outgoing_vertices = graph
+    else:
+        incoming_vertices, outgoing_vertices = gen_graph.gen_graph_vertices(l,w,degree)
+    
+    elapsed = time.time() - start
+    print "%fs (l=%d, w=%d, degree=%d, r=%d)" % (elapsed, l, w, degree, r)
+
+    points = np.arange(l*w)
+    overlaps = [0 for i in range(n)]
+
+    for i in range(n):
+        #generates two items
+        items = {
+            1: set(sample_r_randomly(points, r)),
+            2: set(sample_r_randomly(points, r))
+        }
+        
+        # want to find the set of neurons that are sufficiently connected to item1 and item2
+
+        overlapping_nodes = test_memorization(graph, 1, 2, items, r)
+        if overlapping_nodes:
+            overlaps[i]  = len(overlapping_nodes)
+        else:
+            overlaps[i] = 0
+    return overlaps
+
+
