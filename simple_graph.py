0ff772d84ad40fae1ff98883b343f46b82ed22b6
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2015-05-11 03:36:24 -0400

    Before creating simulations of time steps and associations

--- simple_graph.py
+++ simple_graph.py
@@ -39,6 +39,8 @@ def getPotentialOverlap(fromItem, toItem, incoming_vertices, outgoing_vertices, 
         print "Error"
         return 0
 
+
+
 def testPotentialOverlap(graph=None, l = 100, w = 100, degree = 100, r = 200, n = 1):
     start = time.time()
     
@@ -66,6 +68,67 @@ def testPotentialOverlap(graph=None, l = 100, w = 100, degree = 100, r = 200, n 
     return overlaps
 
 #overlaps = testPotentialOverlap(n=10)
+
+#from the set of vertices, what items are active
+def getActiveItems(vertices, items, threshold=0.8):
+    vertices_set = set(vertices)
+    active_items = []
+
+    for item in items.keys():
+        item_vertices = items[item]
+
+        overlap = vertices_set.intersect(item_vertices)
+
+        #if more than threshold percentage of the neurons in this item
+        #are firing
+        if len(overlap)/len(item_vertices) > threshold:
+            active_items.append(item)
+
+    return active_items
+
+#at every time point of the simulaiton, what are the items that are supposed to be active
+
+#to run simulation
+#create some amount of items from r random vertices, keyed by item id
+#create amount of random associations (from one item id to another item id)
+#function that creates association: turn on this item, keep dictionary of items
+#and items that they are associated with
+def simulation(graph=None, l = 100, w = 100, degree = 100, r = 200, num_items=2, num_associations=2):
+    start = time.time()
+    
+    if graph:
+        incoming_vertices, outgoing_vertices = graph
+    else:
+        incoming_vertices, outgoing_vertices = gen_graph.gen_graph_vertices(l,w,degree)
+    
+    elapsed = time.time() - start
+    print "%fs (l=%d, w=%d, degree=%d, r=%d)" % (elapsed, l, w, degree, r)
+
+
+    points = np.arange(l*w)
+    overlaps = [0 for i in range(n)]
+
+    items = {}
+
+    #holds associations for key fromItem to a list of toItems
+    associations = {}
+
+    #creates random items
+    for i in range(num_items):
+        items[i] = set(sample_r_randomly(points, r))
+
+    #creates random associations between items
+    for i in range(num_associations):
+        fromItem, toItem = np.random.choice(num_items, 2)
+
+        if fromItem not in associations:
+            associations[fromItem] = []
+
+        associations[fromItem].append(toItem)
+
+    
+
+       
 
 
 
